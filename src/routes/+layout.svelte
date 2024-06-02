430efcb1ed39027dd1f718b64a4cdd522423ce0a
CJ Pye <jimmycliff4@gmail.com>
2024-06-02 15:55:27 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { goto } from "$app/navigation";
+  import "../app.css";
+</script>
+
+<!-- Navbar -->
+<div class="bg-base-100 justify-between fixed top-0 left-0 right-0">
+  <div class="navbar max-w-3xl mx-auto justify-between">
+    <!-- left side of navbar-->
+    <div>
+      <a href="/" class="btn btn-ghost text-xl">PokePage</a>
+      <a href="/user" class="btn btn-ghost text-xl">My Page</a>
+    </div>
+    <!-- right side of navbar-->
+    <div>
+      <button on:click={() => goto("/login")}>Login</button>
+      <span class="text-white text-lg ml-2">CJPizzle</span>
+      <button on:click={() => console.log("logging out")}>Logout</button>
+    </div>
+  </div>
+</div>
+
+<slot></slot>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div class="hero min-h-screen bg-base-300">
+  <div class="hero-content">
+    <div class="max-w-md text-center">
+      <h1 class="text-white font-bold text-4xl">Welcome to PokePage</h1>
+      <p class="py-6">
+        Create an acount to make a personal PokePage with your own unique URL!
+      </p>
+      <a href="/login" class="btn btn-primary">Create Account</a>
+    </div>
+  </div>
+</div>
